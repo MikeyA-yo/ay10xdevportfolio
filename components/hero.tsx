196832851bf5aced3a95eef935e064a4b51182cc
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-05-08 17:41:39 +0100

    lgg

---
+++ components/hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero(){
+    return (
+        <>
+        <div className="flex h-screen w-full gap-4 items-center">
+            <div className="flex-[1]"></div>
+            <div></div>
+        </div>
+        </>
+    )
+}
