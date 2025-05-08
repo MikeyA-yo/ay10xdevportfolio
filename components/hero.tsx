03660e33efc35dba8acfeb12550160b7265556e9
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-05-08 18:28:17 +0100

    hero done unto nav

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,44 @@
-export default function Hero(){
-    return (
-        <>
-        <div className="flex h-screen w-full gap-4 items-center">
-            <div className="flex-[1]"></div>
-            <div></div>
+import Image from "next/image";
+import Link from "next/link";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="flex h-screen lg:flex-row flex-col md:flex-row p-10 w-full gap-4 items-center">
+        <div className="flex-[1] w-full flex flex-col gap-3">
+          <h1 className="text-5xl font-bold text-white">
+            HI, I AM
+            <br /> OLUWATOLA AYOMIDE
+          </h1>
+          <p className="text-[#C7C7C7]">
+            A professional fullstack web2 and web3 developer passionate about
+            building great products and solving real-world problems.
+          </p>
+          <div className="flex gap-2">
+            <div className="rounded-full flex gap-2 bg-[#D3E97A] items-center p-2 text-black font-bold cursor-pointer hover:bg-[#C7C7C7] transition-all duration-300 ease-in-out">
+                <p>CONTACT ME</p>
+                <BsArrowUpRightCircleFill />
+            </div>
+            <div className="rounded-full flex items-center justify-center bg-[#222222] p-2 font-bold cursor-pointer hover:bg-[#C7C7C7] transition-all duration-300 ease-in-out">
+                <Link href={"https://x.com/OluwatolaAyomi4"}><FaXTwitter className="h-6 w-6 fill-[#D3E97A]" /></Link>
+            </div>
+            <div className="rounded-full flex items-center justify-center bg-[#222222] p-2 font-bold cursor-pointer hover:bg-[#C7C7C7] transition-all duration-300 ease-in-out">
+                <Link href={"https://github.com/MikeyA-yo"}><FaGithub className="h-6 w-6 fill-[#D3E97A]" /></Link>
+            </div>
+          </div>
         </div>
-        </>
-    )
+        <div className="flex-[1] w-full flex items-center justify-center">
+          <Image
+            src={"/portpic.jpg"}
+            alt="Me"
+            width={500}
+            height={500}
+            className="rounded-md w-96 h-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
