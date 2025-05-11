49568d753ef4a09322718a73dbf42f0dcccf55bd
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-05-11 18:02:06 +0100

    don't stress me

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,14 +2,14 @@
 // import { NextRequest } from "next/server";
 import sendMessage from "./contact";
 
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 
-export async function POST(req:NextRequest, res:NextResponse){
+export async function POST(req:NextRequest){
     try {
         const data = await req.json();
         await sendMessage(data);
-        return  NextResponse.json({ status: "success", data });
+        return  Response.json({ status: "success", data });
     } catch (e) {
         console.log(e);
     }
